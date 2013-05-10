76862235b3cca506b9ceea67375ac3bbc5c006d7
Douglas Miranda <douglasmirandasilva@gmail.com>
2013-05-10 03:03:56 -0400

    unecessary files. for django projects see my repo: /django-twoscoops-project

--- deployment/webfaction/start_gunicorn.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source /home/USER/.virtualenvs/DJANGO_PROJECT_NAME/bin/activate
-cd ~/webapps/DJANGO_PROJECT_NAME
-export DJANGO_SETTINGS_MODULE=DJANGO_PROJECT_NAME.settings.production
-/home/USER/.virtualenvs/DJANGO_PROJECT_NAME/bin/python2.7 manage.py run_gunicorn -c etc/gunicorn.production.conf --daemon --settings=DJANGO_PROJECT_NAME.settings.production && sleep 3
